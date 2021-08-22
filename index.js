5cf5b5e3072ad7a43a27a49fc5d8a52455e09436
jeevan-yohan-varghese <dev.jeevanyohan@gmail.com>
2021-08-23 01:25:04 +0530

    fix: changed port

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ app.use('/auth', authRoutes);
 app.use((err, req, res, next) => {
     res.status(422).send({ error: err.message });
 })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("now listening to requests");
-    console.log(`${process.env.PORT || 3000}`);
+    console.log(`${process.env.PORT || 5000}`);
 });
 
